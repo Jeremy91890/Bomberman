af240bc1c994da9929f83c1400e4c56ec2fd9b98
Galex <agerome@lemonway.com>
2018-07-03 22:43:49 +0200

    little change

--- src/server/game.c
+++ src/server/game.c
@@ -16,29 +16,31 @@ t_game go_logique_server(t_game game, int actual, t_client_request req) {
 }
 
 t_game move_player(t_game game, int actual, t_client_request req) {
-    t_player_infos pi = game.player_infos[actual];
-    int next_dir;
+    // t_player_infos pi = game.player_infos[actual];
+    // int next_dir;
 
-    int current_x = pi.x_pos;
-    int current_y = pi.y_pos;
-    //int current_index = NB_BLOCS_WIDTH * current_y + current_x;
-    
+    // int current_x = pi.x_pos;
+    // int current_y = pi.y_pos;
+    // int current_index = NB_BLOCS_WIDTH * current_y + current_x;
+
 
     int wanted_x = req.x_pos;
     int wanted_y = req.y_pos;
-    int wanted_index = NB_BLOCS_WIDTH * wanted_y + wanted_x;
+    // int wanted_index = NB_BLOCS_WIDTH * wanted_y + wanted_x;
 
-    if (wanted_x - current_x == 1)
-        next_dir = 1;
-    else if (wanted_x - current_x == -1)
-        next_dir = 3;
-    else {
-        if (wanted_y - current_y == 1)
-            next_dir = 2;
-        else if (wanted_y - current_y == -1)
-            next_dir = 4;
-    }
-    game.player_infos[actual].current_dir = next_dir;
+    // if (wanted_x - current_x == 1)
+    //     next_dir = 1;
+    // else if (wanted_x - current_x == -1)
+    //     next_dir = 3;
+    // else {
+    //     if (wanted_y - current_y == 1)
+    //         next_dir = 2;
+    //     else if (wanted_y - current_y == -1)
+    //         next_dir = 4;
+    // }
+    // game.player_infos[actual].current_dir = next_dir;
+
+    game.player_infos[actual].current_dir = req.dir;
 
     if (game.map[wanted_index] == 0b00000111) {
         game.player_infos[actual].x_pos = wanted_x;
